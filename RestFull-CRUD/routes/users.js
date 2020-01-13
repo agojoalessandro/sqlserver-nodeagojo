574c270367ce6c1c5c0e8c3fd3c14104202cceed
agojoalessandro <agojo.alessandro@iisgalvanimi.edu.it>
2020-01-13 10:02:49 +0000

    06.3

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 const sql = require('mssql')
+var createError = require('http-errors');
 
 const config = {
   user: '4DD_01',  //Vostro user name
@@ -39,4 +40,26 @@ router.get('/search/:name', function(req, res, next) {
     });
   });
 });
+
+
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  // Add a new Unit  
+  let unit = req.body;
+  if (!unit) {
+    next(createError(400 , "Please provide a correct unit"));
+  }
+  sql.connect(config, err => {
+    let sqlInsert = `INSERT INTO dbo.[cr-unit-attributes] (Unit,Cost,Hit_Speed) 
+                     VALUES ('${unit.Unit}','${unit.Cost}','${unit.Hit_Speed}')`;
+    let sqlRequest = new sql.Request();
+    sqlRequest.query(sqlInsert, (error, results) => {
+      if (error) throw error;
+      return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
+    });
+  })
+});
+
+
+
 module.exports = router;
