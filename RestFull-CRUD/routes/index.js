4c3f412a364022909322426532fb2cab3b9a4d2f
agojoalessandro <agojo.alessandro@iisgalvanimi.edu.it>
2020-02-10 10:18:51 +0000

    06.3.1 pt 1

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -1,9 +1,45 @@
+
 var express = require('express');
 var router = express.Router();
+const sql = require('mssql')
+var createError = require('http-errors');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const config = {
+  user: '4DD_01',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  //Oppure
+  //server: "213.140.22.237", //nel caso la stringa sopra non funzionasse
+  database: '4DD_01', //(Nome del DB)
+}
+
+
+//Function to connect to database and execute query
+let executeQuery = function (res, query, next) {
+  sql.connect(config, function (err) {
+    if (err) { //Display error page
+      console.log("Error while connecting database :- " + err);
+      res.status(500).json({success: false, message:'Error while connecting database', error:err});
+      return;
+    }
+    var request = new sql.Request(); // create Request object
+    request.query(query, function (err, result) { //Display error page
+      if (err) {
+        console.log("Error while querying database :- " + err);
+        res.status(500).json({success: false, message:'Error while querying database', error:err});
+        sql.close();
+        return;
+      }
+      res.render('unita', { unit : result.recordset}); //Il vettore con i dati è nel campo recordset (puoi loggare result per verificare)
+      sql.close();
+    });
+
+  });
+}
+
+router.get('/', function (req, res, next) {
+  let sqlQuery = "select * from dbo.[cr-unit-attributes]";
+  executeQuery(res, sqlQuery, next);
+}); 
 
 module.exports = router;
