c61499c0453b66a365c322ec458a5afe4b340ef5
Fabio Malizia <45386801+malizia-g@users.noreply.github.com>
2020-04-04 17:48:44 +0200

    Update app.component.ts

--- angular-gitpod/src/app/app.component.ts
+++ angular-gitpod/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
     //Qui va sostituito l’url con quello delle vostre api
     this.obsUnit = this.http.get<Unit[]>('https://3000-fb7f74f9-9abd-445d-a03a-96b52f739433.ws-eu01.gitpod.io/users');
     //Mi sottoscrivo all’observable e scrivo la arrow function che riceve i dati
-    console.log(this.obsUnit.subscribe((data: Unit[]) => {this.data = data;}));
+    console.log(this.obsUnit.subscribe((data: Unit[]) => {this.data = data;})); //Che senso ha loggare qui????
   }
   addUnit(newUnit: HTMLInputElement, newCost: HTMLInputElement, newHitSpeed: HTMLInputElement,newSpeed: HTMLInputElement,newDeployTime: HTMLInputElement,newRange: HTMLInputElement,newTarget: HTMLInputElement,newCount: HTMLInputElement,newTransport: HTMLInputElement,newType: HTMLInputElement,newRarity: HTMLInputElement): boolean {
     let newData: Unit = new Unit(newUnit.value,newCost.value,newHitSpeed.value,newSpeed.value,newDeployTime.value,newRange.value,newTarget.value,newCount.value,newTransport.value,newType.value,newRarity.value);
